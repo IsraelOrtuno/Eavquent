84d77e587b1d756f91754b0a5cd781ec2cb9747d
Israel Ortuno <ai.ortuno@gmail.com>
2015-06-29 18:10:20 +0200

    Added Datetime property type

---
+++ src/Devio/Propertier/Properties/Datetime.php
@@ -0,0 +1,16 @@
+<?php namespace Devio\Propertier\Properties;
+
+use Carbon\Carbon;
+
+class Datetime extends PropertyAbstract {
+
+    public function cast($plainValue)
+    {
+        return new Carbon($plainValue);
+    }
+
+    public function isValidForStorage()
+    {
+        return $this->plainValue instanceof Carbon;
+    }
+}
