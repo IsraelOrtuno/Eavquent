6d7c76096194abb7b50d365337ccf0847317e6da
Israel Ortuno <ai.ortuno@gmail.com>
2015-07-06 09:16:56 +0200

    Registering Choice observer

--- src/Devio/Propertier/Models/PropertyChoice.php
+++ src/Devio/Propertier/Models/PropertyChoice.php
@@ -1,6 +1,7 @@
 <?php namespace Devio\Propertier\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Devio\Propertier\Observers\PropertyChoiceObserver;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PropertyChoice extends Model {
@@ -11,6 +12,16 @@ class PropertyChoice extends Model {
      * @var bool
      */
     public $timestamps = false;
+
+    /**
+     * Booting the model.
+     */
+    protected static function boot()
+    {
+        static::observe(new PropertyChoiceObserver);
+
+        parent::boot();
+    }
 
     /**
      * Relationship the property the choice belongs to.
