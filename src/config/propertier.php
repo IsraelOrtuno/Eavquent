f4ba8dfd11e983d390a76b5104fe3e11d2bb467a
Israel Ortuno <ai.ortuno@gmail.com>
2015-08-26 16:50:19 +0200

    Config commenting

--- src/config/propertier.php
+++ src/config/propertier.php
@@ -2,11 +2,21 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Properties
+    |--------------------------------------------------------------------------
+    |
+    | Here you may set the type of properties available for the Propertier
+    | package. You can register a key value pair which will include the
+    | property plain name as key and the property class path as value.
+    |
+    */
     'properties' => [
-        'integer' => Devio\Propertier\Properties\Integer::class,
-        'choice' => Devio\Propertier\Properties\Choice::class,
-        'Datetime' => Devio\Propertier\Properties\Datetime::class,
-        'String' => Devio\Propertier\Properties\String::class,
+        'integer'  => Devio\Propertier\Properties\Integer::class,
+        'choice'   => Devio\Propertier\Properties\Choice::class,
+        'datetime' => Devio\Propertier\Properties\Datetime::class,
+        'string'   => Devio\Propertier\Properties\String::class,
     ]
 
 ];
