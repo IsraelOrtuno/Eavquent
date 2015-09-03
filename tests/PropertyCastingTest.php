9475221b74a4fef412e222e385b9bc28520509d8
Israel Ortuno <ai.ortuno@gmail.com>
2015-09-03 09:39:08 +0200

    Property casting

---
+++ tests/PropertyCastingTest.php
@@ -0,0 +1,18 @@
+<?php
+
+class PropertyCastingTest extends TestCase
+{
+
+    public function testIntegerCasting()
+    {
+        $this->company->option = 5;
+        $this->assertInternalType('integer', $this->company->option);
+
+        $company = Company::create(['name' => 'foo', 'option' => 5]);
+        $this->assertInternalType('integer', $company->option);
+
+        $company = Company::find($company->id);
+        $this->assertInternalType('integer', $company->option);
+    }
+
+}
