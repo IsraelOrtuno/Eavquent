1841282dfe0967c506b7eab78edf0e3e0de04784
Israel Ortuno <ai.ortuno@gmail.com>
2015-09-03 09:31:44 +0200

    Commenting

--- src/Devio/Propertier/Properties/IntegerProperty.php
+++ src/Devio/Propertier/Properties/IntegerProperty.php
@@ -3,12 +3,23 @@ namespace Devio\Propertier\Properties;
 
 class IntegerProperty extends PropertyAbstract
 {
-
+    /**
+     * Casting the item before accessing.
+     *
+     * @param $plainValue
+     *
+     * @return int
+     */
     protected function cast($plainValue)
     {
         return (int) $plainValue;
     }
 
+    /**
+     * Validating it is a valid integer.
+     *
+     * @return bool
+     */
     public function isValidForStorage()
     {
         return is_integer($this->plainValue);
--- src/Devio/Propertier/Properties/StringProperty.php
+++ src/Devio/Propertier/Properties/StringProperty.php
@@ -3,14 +3,35 @@ namespace Devio\Propertier\Properties;
 
 class StringProperty extends PropertyAbstract
 {
-
+    /**
+     * Decorating before showing.
+     *
+     * @return string
+     */
     public function decorate()
     {
         return (string) $this->plainValue;
     }
 
+    /**
+     * Forcing string casting.
+     *
+     * @param $plainValue
+     *
+     * @return string
+     */
     protected function cast($plainValue)
     {
         return (string) $plainValue;
+    }
+
+    /**
+     * Validating it is a valid string.
+     *
+     * @return bool
+     */
+    public function isValidForStorage()
+    {
+        return is_string($this->plainValue);
     }
 }
