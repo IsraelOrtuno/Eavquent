f60a8c63e5b34ce0771eb52b3746f6829535b3c2
Israel Ortuno <ai.ortuno@gmail.com>
2015-09-07 12:09:18 +0200

    Comenting

--- src/Devio/Propertier/Propertier.php
+++ src/Devio/Propertier/Propertier.php
@@ -6,12 +6,20 @@ use Devio\Propertier\Relations\HasManyProperties;
 
 abstract class Propertier extends Model
 {
+    /**
+     * Relationship to the properties table.
+     *
+     * @return HasManyProperties
+     */
     public function properties()
     {
         $instance = new Property;
 
-        return (new HasManyProperties(
+        // We are using a self coded relation as there is no foreign key into
+        // the properties table. The entity name will be used as a foreign
+        // key to find the properties which belong to this entity item.
+        return new HasManyProperties(
             $instance->newQuery(), $this, $this->getMorphClass()
-        ));
+        );
     }
 }
