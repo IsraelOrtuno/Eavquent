58caf89a80f08cd89987c629307f62dee5d66195
Israel Ortuno <ai.ortuno@gmail.com>
2015-09-11 11:51:48 +0200

    Moving to phpspec

---
+++ src/Devio/Propertier/Finders/PropertyFinder.php
@@ -0,0 +1,73 @@
+<?php
+namespace Devio\Propertier\Finders;
+
+use Devio\Propertier\Property;
+use Illuminate\Support\Collection;
+
+class PropertyFinder
+{
+    /**
+     * Collection of properties to find in.
+     *
+     * @var Collection
+     */
+    protected $properties;
+
+    /**
+     * PropertyFinder constructor.
+     *
+     * @param $properties
+     */
+    public function __construct($properties = null)
+    {
+        $this->properties($properties);
+    }
+
+    /**
+     * Sets the property collection.
+     *
+     * @param $properties
+     *
+     * @return $this
+     */
+    public function properties($properties)
+    {
+        if (is_array($properties))
+        {
+            $properties = collect($properties);
+        }
+
+        $this->properties = $properties;
+
+        return $this;
+    }
+
+    /**
+     * Will return the right property model that matches the key name.
+     *
+     * @param $name
+     *
+     * @return Property
+     */
+    public function find($name)
+    {
+        // We will key our collection by name, this way will be much easier for
+        // filtering. Once keyed, just checking if the property has a key of
+        // the name passed as argument will mean that a property exists.
+        $properties = $this->properties->keyBy('name');
+
+        return $properties->has($name)
+            ? $properties->get($name)
+            : false;
+    }
+
+    /**
+     * Get the properties.
+     *
+     * @return Collection
+     */
+    public function getProperties()
+    {
+        return $this->properties;
+    }
+}
