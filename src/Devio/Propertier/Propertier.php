88b37d5c158f9bacccb02871cc88b7a90ab44259
Israel Ortuño <ai.ortuno@gmail.com>
2015-12-30 10:22:10 +0100

    Renamed to columns

--- src/Devio/Propertier/Propertier.php
+++ src/Devio/Propertier/Propertier.php
@@ -9,11 +9,9 @@ use Illuminate\Database\Eloquent\Relations\MorphMany;
 trait Propertier
 {
     /**
-     * Base model attributes.
-     *
      * @var array
      */
-    protected static $modelAttributes = [];
+    public static $modelColumns = [];
 
     /**
      * Relationship to the properties table.
@@ -22,13 +20,11 @@ trait Propertier
      */
     public function properties()
     {
-        $instance = new Property;
-
         // We are using a self coded relation as there is no foreign key into
         // the properties table. The entity name will be used as a foreign
         // key to find the properties which belong to this entity item.
         return new HasManyProperties(
-            $instance->newQuery(), $this, $this->getMorphClass()
+            (new Property)->newQuery(), $this, $this->getMorphClass()
         );
     }
 
@@ -39,12 +35,17 @@ trait Propertier
      */
     public function values()
     {
+        $this->addHidden('values');
+
         $instance = new Value;
-        list($type, $id) = $this->getMorphs('entity', null, null);
         $table = $instance->getTable();
+        list($type, $id) = $this->getMorphs('entity', null, null);
+        // We will add the values relationship to the list of hidden attributes
+        // as do not want it to be included into any array or json conversion.
+        // We will now just create our custom relation for fetching values.
 
         return new MorphManyValues(
-            $instance->newQuery(), $this, $table . '.' . $type, $table . '.' . $id, $this->getKeyName()
+            $instance->newQuery(), $this, "{$table}.{$type}", "{$table}.{$id}", $this->getKeyName()
         );
     }
 
@@ -59,16 +60,15 @@ trait Propertier
         // Checking if the key corresponds to any comlumn in the main entity
         // table. If there is a match, means the key is an existing model
         // attribute which value will be always taken before property.
-        if (in_array($key, $this->getModelAttributes())) {
+        if (in_array($key, $this->getModelColumns())) {
             return false;
         }
-
         // $key will be property when it does not belong to any relationship
         // name and it also exists into the entity properties collection.
         // This way it won't interfiere with the base model behaviour.
-        return $this->getRelationValue($key)
-            ? false
-            : ! is_null($this->getProperty($key));
+        return is_null($this->getRelationValue($key))
+            ? ! is_null($this->getProperty($key))
+            : false;
     }
 
     /**
@@ -80,7 +80,6 @@ trait Propertier
     public function getProperty($key)
     {
         $properties = $this->getPropertiesRelation();
-
         // We will key our collection by name, this way will be much easier for
         // filtering. Once keyed, just checking if the property has a key of
         // the name passed as argument will mean that a property exists.
@@ -92,7 +91,7 @@ trait Propertier
     /**
      * Will get the values of a property.
      *
-     * @param       $key  Property name
+     * @param  $key Property name
      * @return mixed
      */
     public function getPropertyValue($key)
@@ -101,7 +100,6 @@ trait Propertier
         // We will first grab the property object which contains a collection of
         // values linked to it. It will work even when setting elements that
         // are no yet persisted as they will be set into the relationship.
-
         return $property->values;
     }
 
@@ -114,33 +112,32 @@ trait Propertier
     {
         // This way we can easily swap the name of the relation if needed just
         // in case this relationship name is being used by the parent model
-        // as an attribute or even as relationship.
+        // as an attribute, relationship or other thing that could crash.
         return $this->getRelationValue('properties');
     }
 
     /**
-     * Get the model main attributes.
+     * Get the base model attribute names.
      *
      * @return array
      */
-    public function getModelAttributes()
+    public function getModelColumns()
     {
-        // If no attributes are listed in $modelAttributes property, we will
+        if (empty(static::$modelColumns)) {
+            static::$modelColumns = $this->fetchModelColumns();
+        }
+        // If no attributes are listed into $modelColumns property, we will
         // fetch them from database. This could result into a performance
         // issue so it should be set manually or when booting the model.
-        if (empty(static::$modelAttributes)) {
-            static::$modelAttributes = $this->fetchModelAttributes();
-        }
-
-        return static::$modelAttributes;
+        return static::$modelColumns;
     }
 
     /**
-     * Get the names of the model columns.
+     * Get the model column names.
      *
      * @return mixed
      */
-    public function fetchModelAttributes()
+    public function fetchModelColumns()
     {
         return $this->getConnection()
             ->getSchemaBuilder()
