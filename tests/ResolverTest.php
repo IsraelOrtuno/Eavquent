fe16a7887d064cf4a9a07b1bab9d6f1bff586646
Israel Ortuño <ai.ortuno@gmail.com>
2016-01-12 12:16:15 +0100

    Changed class names

--- tests/ResolverTest.php
+++ tests/ResolverTest.php
@@ -2,8 +2,8 @@
 
 use Devio\Propertier\Property;
 use Devio\Propertier\Resolver;
-use Devio\Propertier\Properties\StringProperty;
-use Devio\Propertier\Properties\IntegerProperty;
+use Devio\Propertier\Values\StringValue;
+use Devio\Propertier\Values\IntegerValue;
 
 class ResolverTest extends PHPUnit_Framework_TestCase
 {
@@ -12,33 +12,37 @@ class ResolverTest extends PHPUnit_Framework_TestCase
     public function setUp()
     {
         Resolver::register([
-            'integer' => IntegerProperty::class,
-            'string' => StringProperty::class
+            'integer' => IntegerValue::class,
+            'string' => StringValue::class
         ]);
         $this->resolver = new Resolver;
     }
 
-    public function test_it_creates_a_property_by_model()
+    /** @test */
+    public function it_resolves_a_property_by_model_instance()
     {
         $property = new Property(['type' => 'string']);
-        $this->assertInstanceOf(StringProperty::class, $this->resolver->property($property));
+        $this->assertInstanceOf(StringValue::class, $this->resolver->value($property));
     }
 
-    public function test_it_creates_a_property_by_string()
+    /** @test */
+    public function it_resolves_a_property_by_string()
     {
-        $this->assertInstanceOf(StringProperty::class, $this->resolver->property('string'));
-        $this->assertInstanceOf(IntegerProperty::class, $this->resolver->property('integer'));
+        $this->assertInstanceOf(StringValue::class, $this->resolver->value('string'));
+        $this->assertInstanceOf(IntegerValue::class, $this->resolver->value('integer'));
     }
 
-    public function test_it_throws_exception_if_no_property_found()
+    /** @test */
+    public function it_throws_exception_if_no_property_found()
     {
         $this->setExpectedException(RuntimeException::class);
-        $this->resolver->property('foo');
+        $this->resolver->value('foo');
     }
 
-    public function test_it_creates_a_property_with_attributes()
+    /** @test */
+    public function it_resolves_a_property_with_attributes()
     {
-        $propertyValue = $this->resolver->property('string', ['value' => 'foo']);
+        $propertyValue = $this->resolver->value('string', ['value' => 'foo']);
         $this->assertEquals(['value' => 'foo'], $propertyValue->toArray());
     }
 }
