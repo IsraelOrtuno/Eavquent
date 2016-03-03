48de8aefd18a3a906924bea4df5547b1e9d88b34
Israel Ortuño <ai.ortuno@gmail.com>
2016-03-03 12:40:57 +0100

    Added more consts

--- src/Devio/Eavquent/Attribute/Attribute.php
+++ src/Devio/Eavquent/Attribute/Attribute.php
@@ -6,10 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Attribute extends Model
 {
-    /**
-     * Column relating attribute to entity.
-     */
+    // Attribute type column
+    const COLUMN_TYPE = 'type';
+    // Entity the attribute belongs to.
     const COLUMN_ENTITY = 'entity';
+    // Attribute default value column.
+    const COLUMN_DEFAULT_VALUE = 'default_value';
 
     /**
      * Model timestamps.
