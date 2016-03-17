3ada4b3454010807b83ee04917576825675db733
Israel Ortuño <ai.ortuno@gmail.com>
2016-03-17 09:30:19 +0100

    Removing unused casting

--- src/Devio/Eavquent/Value/VarcharValue.php
+++ src/Devio/Eavquent/Value/VarcharValue.php
@@ -4,12 +4,4 @@ namespace Devio\Eavquent\Value;
 
 class VarcharValue extends Value
 {
-    /**
-     * Value casting.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'value' => 'string'
-    ];
 }
